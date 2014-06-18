73e00708e4cca835a6c6fbcaa8b358c901371527
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-18 11:37:27 -0400

    These lists are sooooo lazy

--- scratch.hs
+++ scratch.hs
@@ -13,3 +13,5 @@ triangles :: [(Integer, Integer, Integer)]
 triangles = [ (a, b, c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
 
 rightTriangles = [ (a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2 ]
+
+zip [1..] ["apple", "banana", "cherry"]
