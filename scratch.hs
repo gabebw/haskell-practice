32d2b5b59714cd66ab3148b6467580d623899693
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-18 11:36:46 -0400

    Add some list comprehensions

--- scratch.hs
+++ scratch.hs
@@ -1,6 +1,15 @@
 -- Load in GHCI:
 -- :l scratch.hs
+
 doubleMe x = doubled
   where
     doubled = x * two
     two = 2
+
+doubleUs :: Num t => t -> t -> [t]
+doubleUs x y = [doubleMe x, doubleMe y]
+
+triangles :: [(Integer, Integer, Integer)]
+triangles = [ (a, b, c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+
+rightTriangles = [ (a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2 ]
