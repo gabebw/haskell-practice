1737fb1e10e23e0de4edadf58488f98c06ec4d2e
Gabe Berke-Williams <gabebw@gabebw.com>
2014-08-10 21:30:45 -0400

    Add nudge function for shapes

--- custom_types.hs
+++ custom_types.hs
@@ -7,5 +7,12 @@ data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 -- Note that surface takes a Shape - Circle is not a type, but Shape is
 surface :: Shape -> Float
-surface (Circle _ _ r) = pi * r ^ 2
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) dx dy = Circle (Point (x+dx) (y+dy)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) dx dy = Rectangle p1 p2
+  where
+    p1 = Point (x1+dx) (y2+dy)
+    p2 = Point (x2+dx) (y2+dy)
