757d9bc3fd7f9a2be862611ed9cb2e465141c49c
Gabe Berke-Williams <gabebw@gabebw.com>
2014-10-04 15:05:18 -0400

    Add shortLinesOnly

--- capslocker.hs
+++ capslocker.hs
@@ -3,3 +3,10 @@ import Data.Char
 main = do
   contents <- getContents
   putStrLn (map toUpper contents)
+
+shortLinesOnly :: String -> String
+shortLinesOnly input =
+  unlines shortLines
+  where
+    shortLines = filter (\line -> length line < 10) allLines
+    allLines = lines input
