3ec0f565e34941021ffd04798fec5d43ee81d572
Gabe Berke-Williams <gabebw@gabebw.com>
2015-07-29 19:33:06 -0700

    Expand on typeclasses

--- concepts/typeclass.hs
+++ concepts/typeclass.hs
@@ -1,15 +1,19 @@
-module MyTypeClass where
+-- A typeclass defines an interface that types in the typeclass must adhere to.
+-- Here, we define the YesNo typeclass (which is NOT a type).
+-- Types in the YesNo typeclass must define yesno, which takes a parameter of
+-- the same type as the type in the typeclass and returns a Bool.
 
--- Create a custom typeclass that defines a function `yesno
+-- Create a custom typeclass that defines a function `yesno`.
+-- Note that `a` in `class YesNo a` is the SAME `a` as `a -> Bool`!
 class YesNo a where
   yesno :: a -> Bool
 
--- Define how `yesno` applies to Ints
+-- Define how `yesno` applies to integers
 instance YesNo Int where
   yesno 0 = False
   yesno _ = True
 
--- Define how `yesno` applies to arrays
+-- Define how `yesno` applies to lists
 instance YesNo [a] where
   yesno [] = False
   yesno _ = True
@@ -19,9 +23,12 @@ instance YesNo Bool where
   -- Just use the boolean value itself
   yesno = id
 
-instance YesNo (Maybe a) where
+instance YesNo (Maybe _) where
   yesno (Just _) = True
   yesno Nothing = False
 
 yesnoIf :: (YesNo y) => y -> a -> a -> a
-yesnoIf yesnoVal yesnoResult noResult = if (yesno yesnoVal) then yesnoResult else noResult
+yesnoIf yesnoVal yesnoResult noResult =
+    if (yesno yesnoVal)
+    then yesnoResult
+    else noResult
